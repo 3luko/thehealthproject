549a8efad954f472367b6f55ad712a1130e62a21
Ethan <Ethanlukandwa@gmail.com>
2025-01-05 12:32:06 -0600

    Finished calorie intake function, also fixed some bugs

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ goals_calc.addEventListener("click", function () {
   // alert("Would you like to input your gender? This information can be crucial to getting the most accurate understanding of what you need to do to complete your goals!")
   // console.log("Button was clicked!")
   non_priority_alert();
+  console.log(bmr_men());
   console.log("after non-priority function call");
 });
 
@@ -42,10 +43,10 @@ function non_priority_alert() {
   } else {
     //if the inches input is not empty it will change back to red
     if (!isEmpty(height_inches.value)) {
-      document.getElementById("height_inches").style.outline = "none";
+      document.getElementById("height-inches").style.outline = "none";
     }
     if (!isEmpty(height_feet.value)) {
-      document.getElementById("height_feet").style.outline = "none";
+      document.getElementById("height-feet").style.outline = "none";
     }
   }
 }
@@ -60,23 +61,47 @@ function isEmpty(word) {
 
 function bmr_men() {
   //if user submitted without inputting height
+  let height_cm;
+  let weight_kg;
+  let age_bmr;
+
   if (
-    isEmpty(document.getElementById("height-feet")) &&
+    isEmpty(document.getElementById("height-feet")) ||
     isEmpty(document.getElementById("height-inches"))
   ) {
-    let height = (5 * 12 + 9) * 2.54;
-    console.log(height);
+    height_cm = (5 * 12 + 9) * 2.54;
+    //console.log(height);
     //user submitted height
   } else {
-    let height = (document.getElementById("height-feet").value * 12 + 9) * 2.54;
+    height_cm =
+      (Number(document.getElementById("height-feet").value) * 12 +
+        Number(document.getElementById("height-inches").value)) *
+      2.54;
   }
 
-  //if the user did not enter a weight
+  //if the user did not enter a weight, put in the  average weight of American men
   if (isEmpty(document.getElementById("weight"))) {
-    let kg = 199.8 * 0.45359237;
+    weight_kg = 199.8 * 0.45359237;
+    //If the user did input a weight, it uses that and converts it to kilograms
   } else {
-    let kg = document.getElementById("weight").value * 0.45359237;
+    weight_kg = Number(document.getElementById("weight").value) * 0.45359237;
+    console.log("Weight in Kg = " + weight_kg);
   }
 
-  let age = document.getElementById("user-age").value;
+  age_bmr = Number(document.getElementById("user-age").value);
+  console.log("Age = " + age_bmr);
+
+  const bmr = weight_kg * 10 + 6.25 * height_cm - age_bmr * 5 + 5;
+
+  return bmr + " " + document.getElementById("user-name").value;
+}
+
+function daily_cal_deficit() {
+  const calories = Number(document.getElementById("weight-lost").value) * 3500;
+
+  const weeks = Number(document.getElementById("week-amount").value);
+
+  calories = calories / weeks;
+
+  return calories / 7;
 }
