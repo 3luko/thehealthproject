d8d8662162530ddac57de1789890660e48e5eb42
3luko <ethanlukandwa@gmail.com>
2025-09-01 14:09:17 -0500

    Creating Web server with Express and Node.js

--- timer.js
+++ timer.js
@@ -2,23 +2,75 @@ function noNewTab() {
   window.location.href = "home.html";
 }
 
-let time = 
-console.log(time)
-const timerElement = document.getElementById("timer-circle")
+
+const timerId = document.getElementById("timer-circle-id");
+let stopped = false;
+let countdownInterval;
+
+
+const timerCircleElement = document.querySelector(".timer-circle");
+
+
+let timerText = timerCircleElement.textContent;
+let time = parseInt(timerText, 10)
+
+console.log(timerText)
+
 
 function timer(){
     let minutes = Math.floor(time/60);
     let seconds = time % 60;
-    timerElement.textContent = 
+    timerCircleElement.textContent = 
     `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-    if (time > 0){
+    if (time > 0 && !stopped){
         time--;
-    } else{
-        clearInterval(timer);
-        timerElement.textContent = "TIME'S UP"
+    }else if (time <= 0){
+        clearInterval(countdownInterval);
+        timerCircleElement.textContent = "Time's up!";
+    }
+
+    if (time <= 10){
+        timerCircleElement.style.backgroundColor = "red"
     }
 }
 
-timer();
-const timerInterval = setInterval(updateTimer, 1000);
+timerCircleElement.addEventListener("click", function(){
+    stopped = !stopped;
+
+    if(!stopped){
+        countdownInterval = setInterval(timer, 1000);
+        timerCircleElement.style.backgroundColor = "green";
+    } else {
+        clearInterval(countdownInterval);
+        countdownInterval = null;
+        timerCircleElement.style.backgroundColor = "black";
+    }      
+});
+
+function resetTimer(){
+    timerId.textContent = "15";
+    time = 15;
+    stopped = true;
+    clearInterval(countdownInterval);
+    countdownInterval = null;
+    timerCircleElement.style.backgroundColor = "black";
+}
+
+
+function setTimer(){
+    let userInput = prompt("Enter time in seconds:");
+    let newTime = parseInt(userInput, 10);
+    if (!isNaN(newTime) && newTime > 0){
+        timerId.textContent = newTime;
+        time = newTime;
+        stopped = true;
+        clearInterval(countdownInterval);
+        countdownInterval = null;
+        timerCircleElement.style.backgroundColor = "black";
+    }
+}
+
+function startDayTimer(){
+
+}
